28c4c5e9bc1a56f61b100065db5a8c45c913d258
shreenidhi <deshpandeshreenidhi244@gmail.com>
2021-04-14 11:08:17 +0530

    cd

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# compiler-design-code" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/shreenidhisr/compiler-design-code.git
+git push -u origin main
